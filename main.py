319c25735c669b19345d47e66a08bfcaf7941a61
Alexander Cornwall <alex.earl.cornwall@gmail.com>
2026-03-27 23:46:03 +0300

    Updated main.py. Added example logic.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.lib.pagesizes import A4
 from reportlab.lib import colors
 import datetime
+import models
 
 pdfmetrics.registerFont(TTFont('DejaVuSerif', 'DejaVuSerif.ttf'))
 
@@ -46,7 +47,16 @@ class Document:
             leading=12
         )
         
-        self.table_style = TableStyle([
+        self.counetrparty_table_style = TableStyle([
+            ('ALIGN', (0, 0), (-1, -1), 'LEFT'),
+            ('FONTNAME', (0, 0), (-1, -1), 'DejaVuSerif'),
+            ('FONTNAME', (0, 0), (0, -1), 'DejaVuSerif'),
+            ('FONTSIZE', (0, 0), (-1, -1), 10),
+            ('BOTTOMPADDING', (0, 0), (-1, -1), 3),
+        ])
+
+
+        self.invoice_table_style = TableStyle([
             ('BACKGROUND', (0, 0), (-1, 0), colors.grey),
             ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
             ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
@@ -62,25 +72,67 @@ class Document:
         self.doc.build(self.story)        
         print('Created')
 
-    def add_space(self):
-        self.story.append(Spacer(1, 1*cm))
+    def add_space(self, size):
+        self.story.append(Spacer(1, size*cm))
 
     def add_paragraph(self, content, style):
         self.story.append(Paragraph(content, style))
 
     def create_invoice_table(self, items):
         table = Table(items, colWidths=[1*cm, 6*cm, 2*cm, 3*cm, 3*cm])
-        table.setStyle(self.table_style)
+        table.setStyle(self.invoice_table_style)
         self.story.append(table)
-        self.add_space()
+        self.add_space(1)
     
-    def create_contragent_table(self, info):
+    def create_counterparty_table(self, info):
         table = Table(info, colWidths=[3*cm, 6*cm])
-        table.setStyle(self.table_style)
+        table.setStyle(self.counetrparty_table_style)
         self.story.append(table)
-        self.add_space()
+        self.add_space(1)
 
 
 
 if __name__ == "__main__":
-    ...
+    seller = models.Counterparty(       
+        name='ООО "Ромашка"',
+        inn="1234567890",
+        account="40702810000000000000",
+        bank="ПАО Сбербанк",
+        bik="044525225"
+    )
+   
+    buyer = models.Counterparty(       
+        name="ИП Иванов И.И.",
+        inn="987654321012",
+        address="г. Москва, ул. Примерная, д. 1"
+    )
+    
+            
+    items = [
+        models.ServiceItem("1", "Разработка сайта", "1", "50 000", "50 000"),
+        models.ServiceItem("2", "Техническая поддержка", "3", "5 000", "15 000"),
+    ]
+
+    invoice = models.Invoice(
+            invoice_number="164",
+            date="27.03.2026",
+            seller=seller,
+            buyer=buyer,
+            items=items,
+            subtotal="65 000",
+            vat="Без НДС",
+            grand_total="65 000 руб.",
+            )
+
+    doc = Document()
+    doc.add_paragraph(f'СЧЁТ НА ОПЛАТУ № {invoice.invoice_number}', doc.title_style)
+    doc.add_space(0.5)
+
+    doc.create_counterparty_table(seller.to_table_rows())
+    doc.create_counterparty_table(buyer.to_table_rows())
+
+    doc.create_invoice_table(invoice.get_items_table())
+
+    doc.build_doc() 
+
+    
